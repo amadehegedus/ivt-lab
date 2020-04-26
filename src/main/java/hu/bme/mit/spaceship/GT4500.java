6daafc17917c66a1f34ea8c8297b43fd94ed21a4
unknown <amade.hegedus@edu.bme.hu>
2020-04-26 21:14:02 +0200

    return to original GT4500.java file

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,10 +77,9 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        if (!primaryTorpedoStore.isEmpty() && !secondaryTorpedoStore.isEmpty()){
-			primaryTorpedoStore.fire(1);
-			secondaryTorpedoStore.fire(1);
-		}
+        // try to fire both of the torpedo stores
+        //TODO implement feature
+
         break;
     }
 
